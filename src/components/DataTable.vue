362a73722d57e71507fb1c8fa920a9351033437c
Raman Deep Bajwa <dbajwa763@gmail.com>
2022-11-10 20:05:04 -0600

    Add Loader to table

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -13,7 +13,13 @@
                 </template>
                 <template slot="body" v-else>
                     <tbody v-if="columns" class="bi-vue-datatable-tbody">
-                        <tr v-if="(!tableData || !tableData.data || !tableData.data.length)">
+                        <tr v-if="isLoading">
+                            <td :colspan="columns.length" class="bi-vue-loading">
+                                <svg viewBox="0 0 38 38" width="30" height="30"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".25" cx="18" cy="18" r="18"></circle><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.8s" repeatCount="indefinite"></animateTransform></path></g></g></svg>
+                                <span>Loading...</span>
+                            </td>
+                        </tr>
+                        <tr v-else-if="(!tableData || !tableData.data || !tableData.data.length)">
                             <td :colspan="columns.length">No record found.</td>
                         </tr>
                         <template v-else v-for="(item,rowIndex) in tableData.data" :key="item.id">
@@ -82,6 +88,7 @@ export default {
             tableData: {},
             sortKey: 'id',
             sortOrders: {},
+            isLoading: false,
             draw: 0,
             page: 1,
             searchs: [],
@@ -145,6 +152,7 @@ export default {
     methods: {
         async getData(url = this.url,options = this.getRequestPayload){
             this.$emit("loading");
+            this.isLoading = true;
             let baseUrl = url.split("?")[0];
             let response = await axios.get(baseUrl,options).catch(errors => {
                 console.log(errors);
@@ -157,6 +165,7 @@ export default {
                         this.updateParameters(this.tableProps);
                     }
                     this.$emit("finished-loading");
+                    this.isLoading = false;
                 }
             }
         },
